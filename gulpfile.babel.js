8216b134b6d7348ce361998e7fe745543f902f96
Koki Yasuda <Orleika@users.noreply.github.com>
2016-04-09 14:24:42 +0900

    Add task runner (#2)

---
+++ gulpfile.babel.js
@@ -0,0 +1,32 @@
+/* eslint strict: 0, arrow-body-style: 0 */
+'use strict';
+
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import del from 'del';
+import runSequence from 'run-sequence';
+
+const $ = gulpLoadPlugins();
+
+gulp.task('lint', () => {
+  return gulp.src('src/*.js')
+    .pipe($.eslint())
+    .pipe($.eslint.format())
+    .pipe($.eslint.failOnError());
+});
+
+gulp.task('clean', () => del(['bin/*.js']));
+
+gulp.task('build', () => {
+  return gulp.src('src/*.js')
+    .pipe($.babel())
+    .pipe(gulp.dest('bin'));
+});
+
+gulp.task('default', (cb) => {
+  runSequence(
+    'clean',
+    'build',
+    cb
+  );
+});
