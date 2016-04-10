d5ed2062af59075da7621f2dacd7441b57b33c7f
Orleika <orleika@users.noreply.github.com>
2016-04-10 09:38:18 +0000

    Extract cli command options

---
+++ src/cli-options.js
@@ -0,0 +1,63 @@
+const optionDefinitions = [{
+  name: 'list',
+  type: Boolean,
+  group: ['rooms', 'groups', 'splits', 'topics'],
+}, {
+  name: 'create',
+  type: String,
+  group: ['rooms', 'splits'],
+}, {
+  name: 'upload',
+  type: String,
+  group: 'messages',
+}, {
+  name: 'show',
+  type: String,
+  group: ['splits', 'topics'],
+}, {
+  name: 'rooms',
+  type: Boolean,
+  group: 'rooms',
+}, {
+  name: 'messages',
+  type: Boolean,
+  group: 'messages',
+}, {
+  name: 'room',
+  type: String,
+  group: ['messages', 'splits', 'topics'],
+}, {
+  name: 'output',
+  type: String,
+  group: 'messages',
+}, {
+  name: 'splits',
+  type: Boolean,
+  group: 'splits',
+}, {
+  name: 'lang',
+  type: String,
+  group: 'splits',
+}, {
+  name: 'topics',
+  type: Boolean,
+  group: 'topics',
+}, {
+  name: 'help',
+  alias: 'h',
+  type: Boolean,
+}];
+
+const options = {
+  title: 'kakushika-commandline-tool',
+  description: 'Commandline tool for Kakushika',
+  footer: [
+    'Project home: [underline]{https://portal.kksk.io}',
+    'Git: [underline]{https://github.com/Kakushika/kakushika-commandline-tool}',
+  ],
+};
+
+export {
+  optionDefinitions,
+  options,
+};
